c26682eab1961a62b0d7f03ff98398a0b4e5e53b
Yurii Ignatov <Gnatykayura@gmail.com>
2022-08-08 11:48:18 +0300

    added images optimization

--- gulp/config/plugins.js
+++ gulp/config/plugins.js
@@ -2,9 +2,13 @@ import replace from "gulp-replace";
 import plumber from "gulp-plumber";
 import notify from "gulp-notify";
 import browserSync from "browser-sync";
+import newer from "gulp-newer";
+
+//експортируем объекты
 export const plugins = {
     replace: replace,
     plumber: plumber,
     notify: notify,
     browserSync: browserSync,
+    newer: newer,
 };
---
+++ gulp/tasks/images.js
@@ -0,0 +1,31 @@
+import webp from "gulp-webp";
+import imagemin from "gulp-imagemin";
+
+
+
+export const images = () =>{
+    return global.app.gulp.src(global.app.path.src.images, {sourcemaps: true})
+        .pipe(global.app.plugins.plumber(
+            global.app.plugins.notify.onError({
+                title: "IMAGES",
+                message: "Error: <%= error.message %>"
+            }))
+        )
+        .pipe(global.app.plugins.newer(global.app.path.build.images))
+        .pipe(webp())
+        .pipe(global.app.gulp.dest(global.app.path.build.images))
+        .pipe(global.app.gulp.src(global.app.path.src.images))
+        .pipe(global.app.plugins.newer(global.app.path.build.images))
+        .pipe(imagemin(
+          {
+            progressive: true,
+            svgoPlugins: [{ removeViewBox: false }],
+            interlaced: true,
+            optimizationLevel: 3, //0 to 7
+          }  
+        ))
+        .pipe(global.app.gulp.dest(global.app.path.build.images))
+        .pipe(global.app.gulp.src(global.app.path.src.svg))
+        .pipe(global.app.gulp.dest(global.app.path.build.images))
+        .pipe(global.app.plugins.browserSync.stream());
+};
